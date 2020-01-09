7acda0e0c367c8f1fa9e05c3611035b7c5546087
Nuno <ngocalves@gmail.com>
2020-01-09 23:06:44 +0100

    Include react-router-dom and semantic-ui-react

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  HOME: "",
+  RESULTS: "/results",
+  DETAILS: "/detail/:id",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  FORGOT_PASSWORD: "/forgotpassword",
+  CHANGE_PASSWORD: "/changepassword"
+};
