851a27bd77d4232be9eb9b8d24a16acca5a05111
Nuno Goncalves <ngocalves@gmail.com>
2020-01-20 12:37:32 +0100

    Add semantic-ui Container to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Home } from "pages/Home";
+import { Container } from "semantic-ui-react";
 import { AppHeader } from "components/AppHeader";
-//import './App.css'
+import { Home } from "pages/Home";
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <AppHeader />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      {/* 
+    <Container>
+      <AppHeader />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* 
       <Route path='/detail/:id' component={Detail} />
       <Route path='/about' component={About} /> 
       Not found Page
       */}
-    </Switch>
+      </Switch>
+    </Container>
   </BrowserRouter>
 );
 
