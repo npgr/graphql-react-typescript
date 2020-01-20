3b1bab6f94d00bcb10ebd5478aadb91541b8fc28
Nuno Goncalves <ngocalves@gmail.com>
2020-01-20 17:00:09 +0100

    prettier  config

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Menu } from "semantic-ui-react";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
 
 export default () => (
   <>
     <Menu fixed="top">
-      <Menu.Header style={{ margin: "10px" }}>
+      <Menu.Header style={{ margin: '10px' }}>
         <h3 data-test="appTitle">Investment</h3>
       </Menu.Header>
       <Link to="/about">About</Link>
     </Menu>
-    <div style={{ height: "50px" }}></div>
+    <div style={{ height: '50px' }}></div>
   </>
-);
+)
