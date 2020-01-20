6d943d403fd1eb9b751bf480c6d4e861f21919d4
Nuno Goncalves <ngocalves@gmail.com>
2020-01-20 15:35:54 +0100

    style remove semicolons and use single quote

--- src/StyledComponents/Link.ts
+++ src/StyledComponents/Link.ts
@@ -8,8 +8,7 @@ interface linkProps {
 export default styled.div`
   ${({ left }: linkProps) => left && `margin-left: ${left}`};
   display: inline-block;
-  ${({ theme }) => theme && theme.colors && `color: ${theme.colors.blue};`}
-  //theme.colors && theme.colors.blue && 
+  color: ${({ theme }) => theme.colors.blue};
   cursor: pointer;
   :hover {
     text-decoration: underline;
