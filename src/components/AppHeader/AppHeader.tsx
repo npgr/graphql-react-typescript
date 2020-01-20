7e7fb620507eb065fa5b4990b3725531b247858f
Nuno Goncalves <ngocalves@gmail.com>
2020-01-20 12:39:48 +0100

    update AppHeader

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 
 export default () => (
-  <Menu fixed="top">
-    <Menu.Header style={{ margin: "10px" }}>
-      <h3 data-test="appTitle">Search App</h3>
-    </Menu.Header>
-    <Link to="/about">About</Link>
-  </Menu>
+  <>
+    <Menu fixed="top">
+      <Menu.Header style={{ margin: "10px" }}>
+        <h3 data-test="appTitle">Search App</h3>
+      </Menu.Header>
+      <Link to="/about">About</Link>
+    </Menu>
+    <div style={{ height: "50px" }}></div>
+  </>
 );
