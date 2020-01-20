05b8bb52bb2da07708ad0a91672930559801ec8f
Nuno Goncalves <ngocalves@gmail.com>
2020-01-20 17:00:47 +0100

    test

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Icon } from "semantic-ui-react";
-import { Link } from "StyledComponents";
-import { Input } from "./style";
+import React, { useState, useEffect } from 'react'
+import { Icon } from 'semantic-ui-react'
+import { Link } from 'StyledComponents'
+import { Input } from './style'
 
 const SearchForm: React.FC = () => {
-  const [searchValue, setSearchValue] = useState<string>("");
+  const [searchValue, setSearchValue] = useState<string>('')
   useEffect(() => {
     // Component did mount and unmount
-    console.log("call on load component");
-    return () => console.log("leave component");
-  }, []);
-  useEffect(() => console.log("input change:", searchValue), [searchValue]);
+    console.log('call on load component')
+    return () => console.log('leave component')
+  }, [])
+  useEffect(() => console.log('input change:', searchValue), [searchValue])
 
   return (
     <>
@@ -19,14 +19,14 @@ const SearchForm: React.FC = () => {
         //label='Search'
         //size='big'
         icon={<Icon name="search" link onClick={() => alert(searchValue)} />}
-        placeholder="Symbol or company name..."
+        placeholder="Symbol or Company name..."
         onChange={(val: React.FormEvent<HTMLInputElement>) =>
           setSearchValue(val.currentTarget.value)
         }
       />
       <Link left="10px">Advanced options</Link>
     </>
-  );
-};
+  )
+}
 
-export default SearchForm;
+export default SearchForm
